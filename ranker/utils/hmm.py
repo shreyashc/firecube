9991c4e7a21a9bf7b8ad0e15febe9267d70a9cef
shreyashc <shreyashc018@gmail.com>
2020-12-15 13:15:09 +0530

    ..

--- ranker/utils/hmm.py
+++ ranker/utils/hmm.py
@@ -9,7 +9,12 @@ chrome_options.add_argument('headless')
 prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2
                             }}
 chrome_options.add_experimental_option('prefs', prefs)
-browser = webdriver.Chrome(options=chrome_options)
+
+chrome_options.binary_location = GOOGLE_CHROME_PATH
+browser = webdriver.Chrome(
+    execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+
+# browser = webdriver.Chrome(options=chrome_options)
 
 
 def getYtUrl(video_title):
