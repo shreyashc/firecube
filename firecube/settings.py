1290c8a9493ff50d05f477afe0c8264ca9be1945
shreyashc <shreyashc018@gmail.com>
2021-07-03 22:37:04 +0530

    resolve

--- firecube/settings.py
+++ firecube/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
